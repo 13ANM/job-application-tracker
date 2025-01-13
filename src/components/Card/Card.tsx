60d24ea7c76c732fe358f0e1c948dfd65d595148
Andrei Mocanu <andreimcornel@gmail.com>
2025-01-13 21:52:57 +0200

    Have job status as pill

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import { Draggable } from 'react-beautiful-dnd'
-import { Job } from '../../types/board'
+import { Job, Status } from '../../types/board'
 
 interface Props {
 	job: Job
@@ -15,13 +15,27 @@ export const Card = ({ job, index, onClick }: Props) => (
 				ref={provided.innerRef}
 				{...provided.draggableProps}
 				{...provided.dragHandleProps}
-				className={`p-4 bg-white rounded-lg shadow-sm border cursor-pointer ${
+				className={`p-4 bg-white rounded-lg shadow-sm border cursor-pointer space-y-1 ${
 					snapshot.isDragging ? 'bg-blue-50' : ''
 				}`}
 			>
-				<h3 className='font-medium'>{job.title}</h3>
+				<h3 className='font-medium text-base'>{job.title}</h3>
+
 				<p className='text-sm text-gray-500'>{job.company}</p>
-				<p className='text-sm text-blue-500'>{job.status}</p>
+
+				<span
+					className={`inline-block px-2 py-1 text-xs font-semibold rounded-full ${
+						job.status === Status.Scheduled
+							? 'bg-blue-100 text-blue-700'
+							: job.status === Status.AwaitingResponse
+							? 'bg-yellow-100 text-yellow-700'
+							: job.status === Status.Offer
+							? 'bg-green-100 text-green-700'
+							: 'bg-gray-100 text-gray-700'
+					}`}
+				>
+					{job.status}
+				</span>
 			</div>
 		)}
 	</Draggable>
