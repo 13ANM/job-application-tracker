5e403c698afa18ef4686ab71b10671c806743a8b
Andrei Mocanu <andreimcornel@gmail.com>
2025-01-27 18:43:36 +0200

    Move`ConfirmationModal` styles to a separate file

--- src/components/ConfirmationModal/ConfirmationModal.tsx
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -1,4 +1,13 @@
 import { Stage } from '../../types/board'
+import {
+  ConfirmationModalBackdropClassNames,
+  ConfirmationModalButtonContainerClassNames,
+  ConfirmationModalCancelButtonClassNames,
+  ConfirmationModalConfirmButtonClassNames,
+  ConfirmationModalContainerClassNames,
+  ConfirmationModalTextClassNames,
+  ConfirmationModalTitleClassNames
+} from './styles'
 
 interface Props {
   stage: Stage
@@ -7,23 +16,23 @@ interface Props {
 }
 
 export const ConfirmationModal = ({ stage, onConfirm, onCancel }: Props) => (
-  <div className='fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50'>
-    <div className='bg-white p-6 w-96 rounded shadow-md space-y-4'>
-      <h2 className='text-xl font-semibold'>Confirm Erase</h2>
-      <p className='text-sm text-gray-700'>
+  <div className={ConfirmationModalBackdropClassNames}>
+    <div className={ConfirmationModalContainerClassNames}>
+      <h2 className={ConfirmationModalTitleClassNames}>Confirm Erase</h2>
+      <p className={ConfirmationModalTextClassNames}>
         Are you sure you want to remove all items from the{' '}
         <strong>{stage}</strong> column?
       </p>
-      <div className='flex justify-end space-x-2'>
+      <div className={ConfirmationModalButtonContainerClassNames}>
         <button
           onClick={onCancel}
-          className='px-4 py-2 border rounded focus:outline-none hover:bg-gray-100'
+          className={ConfirmationModalCancelButtonClassNames}
         >
           Cancel
         </button>
         <button
           onClick={onConfirm}
-          className='px-4 py-2 bg-red-600 text-white rounded focus:outline-none hover:bg-red-700'
+          className={ConfirmationModalConfirmButtonClassNames}
         >
           Erase
         </button>
---
+++ src/components/ConfirmationModal/styles.ts
@@ -0,0 +1,18 @@
+export const ConfirmationModalBackdropClassNames =
+  'fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50'
+
+export const ConfirmationModalContainerClassNames =
+  'bg-white p-6 w-96 rounded shadow-md space-y-4'
+
+export const ConfirmationModalTitleClassNames = 'text-xl font-semibold'
+
+export const ConfirmationModalTextClassNames = 'text-sm text-gray-700'
+
+export const ConfirmationModalButtonContainerClassNames =
+  'flex justify-end space-x-2'
+
+export const ConfirmationModalCancelButtonClassNames =
+  'px-4 py-2 border rounded focus:outline-none hover:bg-gray-100'
+
+export const ConfirmationModalConfirmButtonClassNames =
+  'px-4 py-2 bg-red-600 text-white rounded focus:outline-none hover:bg-red-700'
